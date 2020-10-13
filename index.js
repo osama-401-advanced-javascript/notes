b351ad6f48d94991106f845175c8a2dd784d0387
Othabteh <o.thabteh@gmail.com>
2020-10-13 17:56:36 +0300

    class-03

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
 'use strict';
+
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
+// const Recored = require('./models/notes-schema.js');
+
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/note';
+mongoose
+    .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((err) => {
+        console.log('NOOOOOOOOOOOOOOOOOOOOOOOOOOO', err.message);
+    });
 
 const arg = new Input();
+// console.log(arg, 'hey im the arg');
+// console.log(Notes);
 
-// console.log(options);
-new Notes(arg)
+const note = new Notes(arg)
+
+note.execute(arg)
+
+// console.log(arg);
